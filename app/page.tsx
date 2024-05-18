46084777d2b4040d6730345e421f3a03a9859d5f
Austin Yau <austin.yau@gmail.com>
2024-05-17 23:58:18 -0700

    clean up imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Welcome from "@/components/Welcome";
 import { SubmitButton } from "./login/submit-button";
 import { signIn } from "@/utils/supabase/signIn";
 import { redirect } from "next/navigation";
